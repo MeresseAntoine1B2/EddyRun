a97b39e443864792b3645b12a479f74379f89c34
Antoine <antoine@meresse.info>
2016-11-29 17:14:51 +0100

    Ajout du saut a l'appui d'une touche et limite les sauts a 1

--- affichage.js
+++ affichage.js
@@ -1,30 +1,27 @@
-function saut(svg, personnage)
-{
-	
-}
+var s = Snap("#svgout");
+var air = false;
+var otherRect = s.rect(50,300,50,50,10,10).attr({ stroke: '#123456', 'strokeWidth': 20, fill: 'green' });
+var rect = s.rect(50,50,150,3);
 
-function animer_long_chemin (path, element, start, dur, callback) 
-{
-  // Get the path length, so we know how many frames we will animate over
-  var len = Snap.path.getTotalLength(path);
-
-  Snap.animate(start, len, function (value) {
-    // movePoint gets the path attributes at a certain frame
-    var movePoint = Snap.path.getPointAtLength(path, value);
-
-    // applies the attributes to our element
-    element.attr({ cx: movePoint.x, cy: movePoint.y });
-  }, dur, mina.easeinout, function () {
-    callback(path);
-  });
+var clickFunc = function () {
+    // reset the animation, may not be needed
+    //otherRect.transform('');
+    if (air == false)
+    {
+	air = true;
+	otherRect.animate({ transform: 't0, -200,0' },400,mina.easein, endAnim );
+    }
 };
 
-var svg = Snap("#svgout");
+var unlock = function ()
+{
+    air = false;
+};
 
-var myPathString = "M 60 0 L 120 0 L 180 60 L 180 120 L 120 180 L 60 180 L 0 120 L 0 60 Z";
-var direct = "m 100,100 L 230,140 m -200,100 h 300";
-var personnage = svg.path( myPathString );
-personnage = personnage.transform("t100,200");
-var dir = svg.path(direct);
+var endAnim = function() {
+    otherRect.animate({ transform: 't0,0,0' }, 350, mina.easeout, unlock );
+}
 
+s.click( clickFunc );
 
+document.onkeydown = clickFunc;
