97f9da3fe208e895358e63560e0ebddab134f221
Antoine <antoine@meresse.info>
2016-12-05 17:16:49 +0100

    Création des obstacles sur le jeu

--- affichage.js
+++ affichage.js
@@ -1,15 +1,15 @@
 var s = Snap("#svgout");
 var air = false;
-var otherRect = s.rect(50,300,50,50,10,10).attr({ stroke: '#123456', 'strokeWidth': 20, fill: 'green' });
+var otherRect = s.rect(50,300,50,50,10,10).attr({ fill: 'green' });
 var rect = s.rect(50,50,150,3);
+var obstacle = s.g();
 
-var clickFunc = function () {
-    // reset the animation, may not be needed
-    //otherRect.transform('');
+var saut = function () 
+{
     if (air == false)
     {
-	air = true;
-	otherRect.animate({ transform: 't0, -200,0' },400,mina.easein, endAnim );
+		air = true;
+		otherRect.animate({ transform: 't0, -200,0' },400,mina.easein, endAnim );
     }
 };
 
@@ -18,10 +18,22 @@ var unlock = function ()
     air = false;
 };
 
-var endAnim = function() {
+var endAnim = function()
+{
     otherRect.animate({ transform: 't0,0,0' }, 350, mina.easeout, unlock );
-}
+};
 
-s.click( clickFunc );
+/**var afficherMap =**/ function test (map) 
+{
+	// crée les obstacles et les groupes pour la translation
+	for (var i = 0; i < map.length; i++) 
+	{
+		if (map[i][1] == 1)
+			obstacle.add(s.rect(75*(i+2),300,50,50).attr({fill: 'red' }));
+		else if (map[i][1] == 2)
+			obstacle.add(s.rect(55*(i+2),300,50,50).attr({fill: 'blue' }));
+	}
+};
 
-document.onkeydown = clickFunc;
+document.onkeydown = saut;
+test(map);
